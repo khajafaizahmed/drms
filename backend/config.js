ab36da765078ba9deb01bbc20b8b7ce0dd528ddd
Faiz Ahmed <41271304+khajafaizahmed@users.noreply.github.com>
2025-07-31 20:44:31 -0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const mysqlConfig = {
-    host: process.env.MYSQLHOST || "localhost",
-    port: process.env.MYSQLPORT || 3306,
-    user: process.env.MYSQLUSER || "testuser",
-    password: process.env.MYSQLPASSWORD || "mypassword",
-    database: process.env.MYSQLDATABASE || "drms",
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
     debug: true
 };
 
