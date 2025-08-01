77658dda9cc048b9354ea27348aa1aef935b9019
Faiz Ahmed <41271304+khajafaizahmed@users.noreply.github.com>
2025-08-01 04:41:16 -0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,16 +1,15 @@
 const mysql = require('mysql2/promise');
 
-const mysqlConfig = {
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    debug: true
-};
-
 async function getDbConnection() {
-    return await mysql.createConnection(mysqlConfig);
+    const connection = await mysql.createConnection({
+        host: process.env.MYSQLHOST,
+        user: process.env.MYSQLUSER,
+        password: process.env.MYSQLPASSWORD,
+        database: process.env.MYSQLDATABASE,
+        port: process.env.MYSQLPORT
+    });
+
+    return connection;
 }
 
 module.exports = { getDbConnection };
